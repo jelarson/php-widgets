d723941294bf702aed8419779972adadde88c9f8
jelarson <jesslarson93@gmail.com>
2020-10-26 17:15:00 -0600

    shuffle widget added

--- widget-page.php
+++ widget-page.php
@@ -38,6 +38,8 @@
     $reverse_answer = (strlen($reverse_text) > 0 ? strrev($reverse_text) : '');
 
     $bg_color = $_POST['bgColor'];
+
+    $shuffle_text = str_shuffle($_POST['shuffle'])
 ?>
   <div class="contact-title">
     <h1>PHP Widget Page</h1>
@@ -50,39 +52,48 @@
       <!-- <input type='submit' class='form-control submit' name="clear" value="clear"> -->
     </form>
   </div>
-  <div class="calculator-widget">
-  <form action="widget-page.php" method="post" action="widget-page.php">
-    First Num: <input type="number" step='0.1' name="num1"> <br>
-    Operator: <select name='op'>
-      <option value='+'>+</option>  
-      <option value='-'>-</option>  
-      <option value='*'>*</option>  
-      <option value='/'>/</option>  
-    <br>
-    Second Num: <input type="number" step='0.1' name="num2"> <br>
-    <input type='submit' value='Submit'>
-  </form>
-  <h2>Answer: <?php echo $calc_answer ?></h2>
-  </div>
-  <div class="reverse-widget">
-  <form action="widget-page.php" method="post" action="widget-page.php">
-    Reverse Text: <input type="text" name="reverse"> <br>
-    <input type='submit' value='Submit'>
-  </form>
-  <h2>Result: <?php echo $reverse_answer ?></h2>
-  </div>
-  <div class="background-widget" style='background: <?php echo $bg_color ?>'>
-  <form action="widget-page.php" method="post" action="widget-page.php">
-  <input type="radio" id="red" name="bgColor" value="red">
-  <label for="red">Red</label><br>
-  <input type="radio" id="purple" name="bgColor" value="purple">
-  <label for="purple">Purple</label><br>
+  <div class='widget-wrapper'>
+
+    <div class="calculator-widget">
+      <form action="widget-page.php" method="post" action="widget-page.php">
+        First Num: <input type="number" step='0.1' name="num1"> <br>
+        Operator: <select name='op'>
+          <option value='+'>+</option>  
+          <option value='-'>-</option>  
+          <option value='*'>*</option>  
+          <option value='/'>/</option>  
+          <br>
+          Second Num: <input type="number" step='0.1' name="num2"> <br>
+          <input type='submit' value='Submit'>
+        </form>
+        <h2>Answer: <?php echo $calc_answer ?></h2>
+      </div>
+      <div class="reverse-widget">
+        <form action="widget-page.php" method="post" action="widget-page.php">
+          Reverse Text: <input type="text" name="reverse"> <br>
+          <input type='submit' value='Submit'>
+        </form>
+        <h2>Result: <?php echo $reverse_answer ?></h2>
+      </div>
+      <div class="background-widget" style='background: <?php echo $bg_color ?>'>
+        <form action="widget-page.php" method="post" action="widget-page.php">
+          <input type="radio" id="red" name="bgColor" value="red">
+          <label for="red">Red</label><br>
+          <input type="radio" id="purple" name="bgColor" value="purple">
+          <label for="purple">Purple</label><br>
   <input type="radio" id="green" name="bgColor" value="green">
   <label for="green">Green</label><br>
     <input type='submit' value='Submit'>
   </form>
-  <h2>Result: <?php echo $bg_color ?></h2>
-  </div>
+</div>
+<div class="shuffle-widget">
+  <form action="widget-page.php" method="post" action="widget-page.php">
+    Shuffle Text: <input type="text" name="shuffle"> <br>
+    <input type='submit' value='Submit'>
+  </form>
+  <h2>Result: <?php echo $shuffle_text ?></h2>
+</div>
+</div>
 
 
 </body>
